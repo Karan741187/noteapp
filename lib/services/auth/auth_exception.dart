77c3235fbb70a0fa9321636798732e3171d5d715
Karan <Karan741187@gmail.com>
2023-08-28 11:57:56 +0530

    step-8

---
+++ lib/services/auth/auth_exception.dart
@@ -0,0 +1,10 @@
+//login exceptions
+ class UserNotFoundAuthException implements Exception {}
+ class WrongPasswordAuthException implements Exception {}
+// Register Exception
+class WeakPasswordAuthException implements Exception {}
+class EmailAlreadyInUseException implements Exception {}
+class InvalidEmailAuthException implements Exception {}
+//generic exceptions
+class GenericAuthException implements Exception{}
+class UserNotLoggedInException implements Exception{}
